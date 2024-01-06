38eb63b5b2926418d122c2a548e0b54abc223079
Chester Lee Coloma <cleecoloma@gmail.com>
2024-01-06 12:45:59 -0800

    added API_KEY env

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ const axios = require('axios');
 
 dotenv.config();
 const PORT = process.env.PORT;
+const API_KEY = process.env.API_KEY;
 
 app.use(bodyParser.json());
 app.use(cors());
@@ -33,30 +34,38 @@ app.put('/create', async (req, res) => {
   }
 });
 
-// app.post('/quest', async (req, res) => {
-//   try {
-//     console.log("HERE'S THE REQUEST FROM FRONTEND:", req.body);
-//     let response;
+app.post('/quest', async (req, res) => {
+  try {
+    console.log("HERE'S THE REQUEST FROM FRONTEND:", req.body);
+    let response;
+    const config = {
+      headers: {
+        'x-api-key': API_KEY,
+      },
+    };
 
-//     if (req.body && Object.keys(req.body).length > 0) {
-//       const requestBody = req.body;
-//       response = await axios.post(
-//         'https://buig0o4co5.execute-api.us-west-2.amazonaws.com/initial/quest-start/1',
-//         requestBody
-//       );
-//     } else {
-//       response = await axios.post(
-//         'https://buig0o4co5.execute-api.us-west-2.amazonaws.com/initial/quest-start/1'
-//       );
-//     }
+    if (req.body && Object.keys(req.body).length > 0) {
+      const requestBody = req.body;
+      response = await axios.post(
+        'https://buig0o4co5.execute-api.us-west-2.amazonaws.com/initial/quest-start/1',
+        requestBody,
+        config
+      );
+    } else {
+      response = await axios.post(
+        'https://buig0o4co5.execute-api.us-west-2.amazonaws.com/initial/quest-start/1',
+        {},
+        config
+      );
+    }
 
-//     console.log("HERE'S THE RESPONSE:", response.data);
-//     res.json(response.data);
-//   } catch (error) {
-//     console.error('Error processing the request:', error);
-//     res.status(500).send('Internal Server Error');
-//   }
-// });
+    console.log("HERE'S THE RESPONSE:", response.data);
+    res.json(response.data);
+  } catch (error) {
+    console.error('Error processing the request:', error);
+    res.status(500).send('Internal Server Error');
+  }
+});
 
 app.listen(PORT, () => {
   console.log(`Server is running on PORT ${PORT}`);
